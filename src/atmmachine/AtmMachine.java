19cbbf5d816a3b6c2b0ae60d3eda8595baff6dfc
BISHAL BHANDARI <80243759+James-smith2@users.noreply.github.com>
2022-01-24 17:18:57 +0545

    main

---
+++ src/atmmachine/AtmMachine.java
@@ -0,0 +1,17 @@
+
+package atmmachine;
+
+/**
+ *
+ * @author Dell
+ */
+public class AtmMachine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
+    
+}
